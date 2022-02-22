7251c00597c75241a0767ac0e059549718d75baa
vsoch <vsoch@users.noreply.github.com>
2022-02-22 16:56:25 -0700

    force create
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/create_pages.sh
+++ .github/scripts/create_pages.sh
@@ -12,11 +12,6 @@ git branch
 git config --global user.name "github-actions"
 git config --global user.email "github-actions@users.noreply.github.com"
 
-# We haven't created the site yet
-if  [[ ! -d "docs" ]]; then
-    git add .
-    git commit -m "Automated deployment of pages! $(date '+%Y-%m-%d')" || exit 0
-    git push origin "${BRANCH_FROM}" --force || exit 0
-else
-    printf "This site has already been created. Use the update workflow instead.\n"
-fi
+git add .
+git commit -m "Automated deployment of pages! $(date '+%Y-%m-%d')" || exit 0
+git push origin "${BRANCH_FROM}"
